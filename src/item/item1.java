4766c2a126ff2b37746051002342f8588cf3729a
sungbumchoi <46594664+sungbumchoi@users.noreply.github.com>
2022-11-20 10:48:23 +0900

    feat : 야이템 1
    야이템 1

---
+++ src/item/item1.java
@@ -0,0 +1,11 @@
+package item;
+
+public class item1 {
+	int i =0; 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author csbti
+ *
+ */
+module EffectiveJava {
+}
