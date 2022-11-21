9084d1403def2b620a1c58041b7db978b83f1ce6
sungbumchoi <46594664+sungbumchoi@users.noreply.github.com>
2022-11-21 15:55:55 +0900

    feat : chapter2 소스반영
     chapter2 소스반영

---
+++ src/chapter1/item2/Item2.java
@@ -0,0 +1,10 @@
+package chapter1.item2;
+
+public class Item2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/chapter1/item2/hierarchicalbuilder/PizzaTest.java
@@ -0,0 +1,16 @@
+package chapter1.item2.hierarchicalbuilder;
+
+import static chapter1.item2.hierarchicalbuilder.Pizza.Topping.*;
+
+// 계층적 빌더 사용 (21쪽)
+public class PizzaTest {
+    public static void main(String[] args) {
+//        NyPizza pizza = new NyPizza.Builder(SMALL)
+//                .addTopping(SAUSAGE).addTopping(ONION).build();
+//        Calzone calzone = new Calzone.Builder()
+//                .addTopping(HAM).sauceInside().build();
+//        
+//        System.out.println(pizza);
+//        System.out.println(calzone);
+    }
+}
